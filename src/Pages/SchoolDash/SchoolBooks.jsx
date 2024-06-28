f938e2b4cf4eaa3bd42f5331a5be0674ed91fbb1
arpitgupta006 <65354384+arpitgupta006@users.noreply.github.com>
2024-06-28 15:26:45 +0530

    Add files via upload

---
+++ src/Pages/SchoolDash/SchoolBooks.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../Styles/Dashboard.css'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { useNavigate } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function SchoolBooks() {
+
+    const navigate = useNavigate();
+  return (
+    <div>
+         <div className='dashboarddiv'>
+        <h1>SCHOOL DASHBOARD</h1>
+      </div>
+
+      <Row>
+        <Col lg={3} sm={12}>
+          <Sidebar className=''>
+
+            <Menu>
+            <MenuItem onClick={()=>navigate('/dashboard-schoolbooks')}> Books Assigned </MenuItem>
+              <MenuItem onClick={()=>navigate('/dashboard-schoolstudents')}> Students </MenuItem>
+              <MenuItem onClick={()=>navigate('/dashboard-schoolteachers')}> Teachers </MenuItem>
+            </Menu>
+          </Sidebar>
+        </Col>
+
+        <Col lg={3} sm={12}>
+        <h1>Books Assigned</h1>
+        </Col>
+
+        <Col lg={3} sm={12}>
+        </Col>
+
+        <Col lg={3} sm={12}>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default SchoolBooks
---
+++ src/Pages/SchoolDash/SchoolStudents.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import '../Styles/Dashboard.css'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { useNavigate } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function SchoolStudents() {
+
+    const navigate = useNavigate();
+
+  return (
+    <div>
+         <div className='dashboarddiv'>
+        <h1>SCHOOL DASHBOARD</h1>
+      </div>
+
+      <Row>
+        <Col lg={3} sm={12}>
+          <Sidebar className=''>
+
+            <Menu>
+            <MenuItem onClick={()=>navigate('/dashboard-schoolbooks')}> Books Assigned </MenuItem>
+              <MenuItem onClick={()=>navigate('/dashboard-schoolstudents')}> Students </MenuItem>
+              <MenuItem onClick={()=>navigate('/dashboard-schoolteachers')}> Teachers </MenuItem>
+            </Menu>
+          </Sidebar>
+        </Col>
+
+        <Col lg={3} sm={12}>
+         <h1>School Student</h1>
+        </Col>
+
+        <Col lg={3} sm={12}>
+       
+        </Col>
+
+        <Col lg={3} sm={12}>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default SchoolStudents
